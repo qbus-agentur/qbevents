df462304699a7e9bfea511c6c1327f856c8aa152
Benjamin Franzke <bfr@qbus.de>
2016-02-05 13:02:36 +0100

    Add missing table definitions

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,6 +6,9 @@ CREATE TABLE tx_qbevents_domain_model_event (
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 
+	title varchar(255) DEFAULT '' NOT NULL,
+	location varchar(255) DEFAULT '' NOT NULL,
+	teaser text NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
