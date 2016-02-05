904138fb57109ae22ec246a87e8023811f4c2148
Benjamin Franzke <bfr@qbus.de>
2016-02-05 12:39:14 +0100

    init

---
+++ ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Event Management',
+    'description' => '',
+    'category' => '',
+    'author' => 'Benjamin Franzke',
+    'author_email' => 'bfr@qbus.de',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.0-7.6.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    'autoload' => array(
+        'psr-4' => array(
+            'Qbus\\Qbevents\\' => 'Classes',
+        ),
+    ),
+);
