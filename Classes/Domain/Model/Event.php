7a9d7d2c7bba103802f12f7cc53dc9682c995075
Benjamin Franzke <bfr@qbus.de>
2016-02-05 12:39:49 +0100

    Add empty Event model

---
+++ Classes/Domain/Model/Event.php
@@ -0,0 +1,9 @@
+<?php
+namespace Qbus\Qbevents\Domain\Model;
+
+/**
+ * Event
+ */
+class Event extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
+{
+}
--- ext_tables.php
+++ ext_tables.php
@@ -2,3 +2,9 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_qbevents_domain_model_event');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+    'tx_qbevents_domain_model_event',
+    'EXT:qbevents/Resources/Private/Language/locallang_csh_tx_qbevents_domain_model_event.xlf'
+);
