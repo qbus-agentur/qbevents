01d899b9641740b74fe1879dc638cfe46f75f57e
Benjamin Franzke <bfr@qbus.de>
2016-02-05 12:47:44 +0100

    Add model EventDate

---
+++ Classes/Domain/Model/EventDate.php
@@ -0,0 +1,9 @@
+<?php
+namespace Qbus\Qbevents\Domain\Model;
+
+/**
+ * EventDate
+ */
+class EventDate extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
+{
+}
--- ext_tables.php
+++ ext_tables.php
@@ -8,3 +8,9 @@ if (!defined('TYPO3_MODE')) {
     'tx_qbevents_domain_model_event',
     'EXT:qbevents/Resources/Private/Language/locallang_csh_tx_qbevents_domain_model_event.xlf'
 );
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_qbevents_domain_model_eventdate');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+    'tx_qbevents_domain_model_eventdate',
+    'EXT:qbevents/Resources/Private/Language/locallang_csh_tx_qbevents_domain_model_eventdate.xlf'
+);
