62af579f58ab40e1fca136a2ddc613a028b1f4cb
Benjamin Franzke <bfr@qbus.de>
2016-05-11 10:35:41 +0200

    EventDateRepository: extend findUpcoming to allow a $demands and $limit restriction
    $demands can be expressed as an array representation.
    
    e.g. to find events that have the location "Foo" and are in category uid 2 or 3:
    
    $demands = array(
    	array(
    		'operation' => 'EQUALS',
    		'property' => 'event.location',
    		'value' => 'Foo',
    	),
    	array(
    		'operation' => 'OR'
    		'operands'  => array(
    			array(
    				'operation' => 'CONTAINS',
    				'property' => 'event.categories',
    				'value' => '3',
    			),
    			array(
    				'operation' => 'CONTAINS',
    				'property' => 'event.categories',
    				'value' => '4',
    			),
    		),
    	),
    );

--- Classes/Domain/Repository/EventDateRepository.php
+++ Classes/Domain/Repository/EventDateRepository.php
@@ -1,6 +1,8 @@
 <?php
 namespace Qbus\Qbevents\Domain\Repository;
 
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+
 /**
  * EventDateRepository
  *
@@ -39,17 +41,95 @@ class EventDateRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
         return $query->execute();
     }
 
-    public function findUpcoming()
+
+    public function findUpcoming($demands, $limit)
     {
         $query = $this->createQuery();
 
-        $query->matching(
-            $query->logicalAnd(
-                $query->greaterThan('start', new \DateTime('NOW')),
-                $query->equals('frequency', 0)
-            )
+        $constraints = array(
+            $query->greaterThan('start', new \DateTime('NOW')),
+            $query->equals('frequency', 0),
         );
 
+        $additional = $this->getConstraintsForDemand($query, $demands);
+        if ($additional !== null) {
+            $constraints[] = $additional;
+        }
+
+        $query->matching($query->logicalAnd($constraints));
+
+        if ($limit) {
+            $query->setLimit((int) $limit);
+        }
+
         return $query->execute();
+    }
+
+    /**
+     * Get a QueryInterface constraint from an array definition
+     *
+     * @param QueryInterface $query
+     * @param array          $demands
+     * @param string         $conjunction
+     *
+     * @return void
+     */
+    protected function getConstraintsForDemand($query, $demands, $conjunction = 'AND')
+    {
+        $constraints = array();
+
+        if (!is_array($demands) || empty($demands)) {
+            return null;
+        }
+
+        foreach ($demands as $key => $demand) {
+            if (!isset($demand['demand']))
+                continue;
+            $constraint = $demand['demand'];
+
+            switch ($constraint['operation']) {
+            case 'EQUALS':
+                $constraints[] = $query->equals($constraint['property'], $constraint['value']);
+                break;
+            case 'LIKE':
+                $constraints[] = $query->like($constraint['property'], $constraint['value']);
+                break;
+            case 'CONTAINS':
+                $constraints[] = $query->contains($constraint['property'], $constraint['value']);
+                break;
+            case 'LESSTHAN':
+                $constraints[] = $query->lessThan($constraint['property'], $constraint['value']);
+                break;
+            case 'LESSTHANOREQUAL':
+                $constraints[] = $query->lessThanOrEqual($constraint['property'], $constraint['value']);
+                break;
+            case 'GREATERTHAN':
+                $constraints[] = $query->greaterThan($constraint['property'], $constraint['value']);
+                break;
+            case 'GREATERTHANOREQUAL':
+                $constraints[] = $query->greaterThanOrEqual($constraint['property'], $constraint['value']);
+                break;
+            case 'AND':
+                $constraints[] = $this->getConstraintsForDemand($query, $constraint['operands'], 'AND');
+                break;
+            case 'OR':
+                $constraints[] = $this->getConstraintsForDemand($query, $constraint['operands'], 'OR');
+                break;
+            default:
+                return null;
+            }
+        }
+
+        $result = null;
+        switch ($conjunction)  {
+        case 'AND':
+            $result = $query->logicalAnd($constraints);
+            break;
+        case 'OR':
+            $result = $query->logicalOr($constraints);
+            break;
+        }
+
+        return $result;
     }
 }
