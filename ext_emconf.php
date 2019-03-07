eda77a24dcdc6fd7b0ddf47e524ba738a15599b8
Benjamin Franzke <bfr@qbus.de>
2019-03-07 22:17:39 +0100

    Define to be compatible with TYPO3 v9

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.10.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-8.7.99',
+            'typo3' => '6.2.0-9.5.99',
         ),
         'conflicts' => array(
         ),
